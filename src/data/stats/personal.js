411efa147d72be4505792f5ec86d2e3a98de8053
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-05-31 12:38:38 +0200

    Reduce number of digits for current age

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -6,7 +6,7 @@ const Age = () => {
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
     const birthTime = new Date('1995-08-13T19:30:00');
-    setAge(((Date.now() - birthTime) / divisor).toFixed(11));
+    setAge(((Date.now() - birthTime) / divisor).toFixed(8));
   };
 
   useEffect(() => {
