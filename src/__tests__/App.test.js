7ee493ad9b24f47a293297fd028011664d7a63ba
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-06-08 12:37:09 +0200

    Fix linting errors

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Florian Kotthoff");
+    expect(document.title).toBe('Florian Kotthoff');
   });
 
   it('can navigate to /resume', async () => {
