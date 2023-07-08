5d952284acf0659a22fe8f267938a3577137a17d
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-08 13:03:30 +0200

    Add openpv.de as project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,18 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'openpv.de',
+    subtitle: 'Simulating the solar potential of your building in the browser',
+    image: '/images/projects/openpv.jpg',
+    link: 'https://www.openpv.de',
+    date: '2023-06-01',
+    desc:
+      'Together with two friends I built a website to simulate the solar '
+      + 'potential of roofs and facades using openly available 3D building data. '
+      + 'Right now we can simulate every building in Bavaria and we are working on '
+      + 'extending to other states as well.',
+  },
+  {
     title: 'open-mastr python package',
     subtitle: 'A collaborative software to download the energy database Marktstammdatenregister (MaStR)',
     image: '/images/projects/openmastr.jpg',
