085cca0f6231aa927acaf2846d8c516b19a1e177
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-17 09:12:12 +0200

    Add Erasmus in Edinburgh to degrees

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -6,6 +6,12 @@ const degrees = [
     year: 2021,
   },
   {
+    school: 'University of Edinburgh',
+    degree: 'Erasmus Semester in Physics',
+    link: 'https://www.ph.ed.ac.uk/',
+    year: 2017,
+  },
+  {
     school: 'Technical University of Munich',
     degree: 'B.Sc. Physics',
     link: 'https://tum.de',
